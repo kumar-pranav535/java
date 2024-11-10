a05465245ad58dcd345cb9bfca04450b093702af
kumar-pranav535 <kumarpranav535@gmail.com>
2024-11-10 18:11:19 +0530

    first message

---
+++ Chapters/4 variables/codes/scripts/index.js
@@ -0,0 +1,35 @@
+let welcomeMessage = 'Hello Friends';
+console.log('hello friends')
+console.log(welcomeMessage)
+console.log(welcomeMessage)
+//let mat_@ch = 77;
+/*let a = 3;
+let b = 4;
+console.log(a+b)
+//let a = 3 let a = 3
+console.log(a+b);*/
+let a = 5;
+let b = 6;
+let c = a + 4;
+console.log(c + 1);
+console.log(c * 2);
+let i = 5;
+//i = i + 5;
+/*i += 5;
+i -= 5;
+i *= 2;
+i /= 2;
+i++;
+console.log(i);*/
+
+const pi = 3.14159265359;
+let radius = 4;
+//pi = 2;
+console.log(pi * radius * radius);
+
+
+
+
+
+
+
